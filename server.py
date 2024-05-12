fe34a834184980dd02c768df08c11684623a5f3f
Jai Madera <onesthathumbled@gmail.com>
2024-05-12 22:10:41 +0800

    changed the location of the bot

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route('/execute-python', methods=['POST'])
 def execute_python():
-    script_path = './bots.py'
+    script_path = 'bots.py'
     try:
         result = subprocess.check_output(['python', script_path], stderr=subprocess.STDOUT, text=True)
         return jsonify({'output': result})
