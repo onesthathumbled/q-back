caeefaaef01f66f6c185dc8a81690da8cb4ddc0f
Jai Madera <onesthathumbled@gmail.com>
2024-05-12 22:17:39 +0800

    added asyncio for asynchronous request

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import subprocess
+import asyncio
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/execute-python', methods=['POST'])
-def execute_python():
-    script_path = 'bots.py'
+async def execute_python():
+    script_path = './bots.py'
     try:
-        result = subprocess.check_output(['python', script_path], stderr=subprocess.STDOUT, text=True)
+        # Use asyncio.subprocess to run the command asynchronously
+        process = await asyncio.create_subprocess_exec('python', script_path,
+                                                       stdout=asyncio.subprocess.PIPE,
+                                                       stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await process.communicate()
+        result = stdout.decode()  # Decode the output bytes to a string
         return jsonify({'output': result})
     except subprocess.CalledProcessError as e:
         return jsonify({'error': str(e.output)})
